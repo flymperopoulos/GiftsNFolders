aa0e320e10677ceb07a0c19c01b3091d09e733ce
flymperopoulos <filippos.lymperopoulos@students.olin.edu>
2015-03-05 09:22:39 -0500

    integration with indexing

--- public/javascripts/root.js
+++ public/javascripts/root.js
@@ -1,7 +1,54 @@
 angular.module('mainApp', ['ngDraggable']).
   controller('MainCtrl', function ($scope) {
+
+    $scope.items = [];
+    $scope.currFolderName  = "";
+    $scope.folders = [{
+            folderName : "firstFolder",
+            itemsContained : []
+        }];
+
+    $scope.defaultFolder = {
+        folderName: "Default",
+        itemsContained: []
+    };
+    
+    $scope.defaultContent = false;
+    $scope.folderContent = false;
+      
+    $scope.createItem = function () {  
+        $scope.showTable = true;
+        
+        var newItem = {
+            itemKeyName : $scope.nameForNewItem,
+            itemKeyCost : $scope.costForNewItem, 
+            foldersItBelongsTo : [$scope.defaultFolder.folderName]
+        }
+        
+        $scope.items.push(newItem);
+        $scope.defaultFolder.itemsContained.push(newItem);
+        $scope.nameForNewItem = null;
+        $scope.costForNewItem = null;
+    };
+    
+    // Creates new folder
+    $scope.createFolder = function () { 
+        
+        var newFolder = {
+            folderName : $scope.nameForNewFolder,
+            itemsContained : []
+        }       
+        
+        $scope.folders.push(newFolder);
+        $scope.nameForNewFolder= null;
+    };
+
     $scope.centerAnchor = true;
-    $scope.toggleCenterAnchor = function () {$scope.centerAnchor = !$scope.centerAnchor}
+
+    $scope.toggleCenterAnchor = function () {
+        $scope.centerAnchor = !$scope.centerAnchor
+    }
+
     $scope.draggableObjects = [{name:'one'}, {name:'two'}, {name:'three'}];
     $scope.droppedObjects1 = [];
     $scope.droppedObjects2= [];
@@ -32,4 +79,10 @@ angular.module('mainApp', ['ngDraggable']).
     var inArray = function(array, obj) {
         var index = array.indexOf(obj);
     }
+
+
+    //shows Default content
+    $scope.showDefaultFolderGifts = function (){
+        $scope.defaultContent =  !$scope.defaultContent;
+    } 
 });
